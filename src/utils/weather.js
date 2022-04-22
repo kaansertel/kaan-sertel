747ac2a09b6f74671ccde01bff80a2a10822b654
unknown <kaan_sertel@hotmail.com>
2022-04-22 10:58:09 +0300

    weather

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,2 +1,9 @@
 const request = require('request')
 
+const url = 'http://api.weatherstack.com/current?access_key=964530bb145f5bddbd6d636ccbd89fd7&query=40.1917,29.0611'
+
+
+const weather = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=964530bb145f5bddbd6d636ccbd89fd7&query=' + latitude + ',' + longitude + '&units=m'
+
+
