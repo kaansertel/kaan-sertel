6e1beafc1845b2fced918f76b5f4f62377dc004d
unknown <kaan_sertel@hotmail.com>
2022-04-22 10:59:12 +0300

    weather update

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -7,3 +7,23 @@ const weather = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=964530bb145f5bddbd6d636ccbd89fd7&query=' + latitude + ',' + longitude + '&units=m'
 
 
+    request({ url: url, json:true}, (error, body) => {
+
+        if (error){
+
+            callback('Error', undefined )
+        }else if(body.error){
+
+            callback('Error', undefined)
+        }else {
+
+            callback(undefined, ' Hava Sıcaklığı ' + body.current.temperature )
+            
+        }
+    })
+}
+
+
+
+
+module.exports = weather
